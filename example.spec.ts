a5f63f7c61239293e654c294d9d8928e8ff782b9
Marco Emrich <marco.emrich@codecentric.de>
2022-07-06 09:36:32 +0200

    refactor: simpler unwrap

--- example.spec.ts
+++ example.spec.ts
@@ -1,18 +1,17 @@
 import { err, Ok, ok, Result } from "neverthrow";
 
+const reciprocal: (x: number) => Result<number, string> = (x: number) =>
+  x == 0.0 ? err("Zero has no Reciprocal") : ok(1.0 / x);
+
+const cube1 = (x: number) => x * x * x;
+
+// Js.log(cube1(reciprocal(3.0)));
+
+const cube2 = (x: Result<number, string>): Result<number, string> => {
+  return x.isOk() ? ok(x.value * x.value * x.value) : err("can calc");
+};
+
 test("should calc", () => {
-  const reciprocal: (x: number) => Result<number, string> = (x: number) =>
-    x == 0.0 ? err("Zero has no Reciprocal") : ok(1.0 / x);
-
-  const cube1 = (x: number) => x * x * x;
-
-  // Js.log(cube1(reciprocal(3.0)));
-
-  const cube2 = (x: Result<number, string>): Result<number, string> => {
-    if (x.isOk()) return ok(x.unwrapOr(0) * x.unwrapOr(0) * x.unwrapOr(0));
-    else return err("can calc");
-  };
-
   cube2(reciprocal(2)); /* ? */
 
   cube1(reciprocal(2).unwrapOr(1)); /* ? */
